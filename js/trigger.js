4f843433dbc90059178a4cf1f4e04dff1e756607
King Sway <knucles0906@live.com>
2018-04-07 18:10:26 -0300

    Melhorias no código e adicionamento de recursos
    Adicionei uma barra de pesquisa;
    Mudei o conteúdo de algumas tabelas;
    Adicionei novo ícone para o site;
    Gatilhos do Javascript em um documento só (trigger.js)
    Nova página adicionada, Projetos.html: é uma forma para eu não esquecer as novas funções que quero adicionar. Obviamente que no projeto final isso some

---
+++ js/trigger.js
@@ -0,0 +1,4 @@
+$('.dropdown-trigger').dropdown({hover: false,constrainWidth: false,coverTrigger: false});
+$(document).ready(function(){$('.modal').modal();});
+$(document).ready(function(){$('select').formSelect();});
+$(document).ready(function(){$('.tooltipped').tooltip();});
